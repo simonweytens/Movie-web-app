8839b657a75c1b8c12ccc2ccc55d48ab0e21c292
Simon Weytens <simon.weytens@student.vives.be>
2021-10-20 18:23:33 +0200

    Add project files.

---
+++ Movie web app/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Movie_web_app.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+      
+        //Get: /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: /HelloWorld/Welcome
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        
+    }
+}
---
+++ Movie web app/Data/Movie_web_appContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movie_web_app.Models;
+
+namespace Movie_web_app.Data
+{
+    public class Movie_web_appContext : DbContext
+    {
+        public Movie_web_appContext (DbContextOptions<Movie_web_appContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie_web_app.Models.Movie> Movie { get; set; }
+    }
+}
